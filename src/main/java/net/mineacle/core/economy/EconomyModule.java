b8a02f9474c625cac4d5b4265f5d013881f45d89
Inhaler <brandinsarazin@gmail.com>
2026-04-27 22:22:18 -0500

    update

--- src/main/java/net/mineacle/core/economy/EconomyModule.java
+++ src/main/java/net/mineacle/core/economy/EconomyModule.java
@@ -1,5 +1,6 @@
 package net.mineacle.core.economy;
 
+import net.milkbowl.vault.economy.Economy;
 import net.mineacle.core.Core;
 import net.mineacle.core.bootstrap.Module;
 import net.mineacle.core.economy.command.BalanceCommand;
@@ -7,13 +8,18 @@ import net.mineacle.core.economy.command.EcoCommand;
 import net.mineacle.core.economy.command.PayCommand;
 import net.mineacle.core.economy.listener.EconomyJoinListener;
 import net.mineacle.core.economy.service.EconomyService;
+import net.mineacle.core.economy.vault.MineacleVaultEconomy;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.plugin.ServicePriority;
 
 public final class EconomyModule extends Module {
 
     private static EconomyService economyService;
+
+    private MineacleVaultEconomy vaultEconomy;
 
     public static EconomyService economyService() {
         return economyService;
@@ -40,15 +46,40 @@ public final class EconomyModule extends Module {
                 new EconomyJoinListener(core, economyService),
                 core
         );
+
+        registerVaultProvider(core);
     }
 
     @Override
     public void disable() {
+        if (vaultEconomy != null) {
+            Bukkit.getServicesManager().unregister(Economy.class, vaultEconomy);
+            vaultEconomy = null;
+        }
+
         if (economyService != null) {
             economyService.save();
         }
 
         economyService = null;
+    }
+
+    private void registerVaultProvider(Core core) {
+        if (Bukkit.getPluginManager().getPlugin("Vault") == null) {
+            core.getLogger().warning("Vault is not installed. Mineacle economy will not be registered with Vault.");
+            return;
+        }
+
+        this.vaultEconomy = new MineacleVaultEconomy(economyService);
+
+        Bukkit.getServicesManager().register(
+                Economy.class,
+                vaultEconomy,
+                core,
+                ServicePriority.Highest
+        );
+
+        core.getLogger().info("Registered MineacleCore as the Vault economy provider.");
     }
 
     private void register(Core core, String commandName, CommandExecutor executor) {
